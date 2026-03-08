8901b7e20161ddfc7a69a2966563289783c5ca24
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-03-08 22:19:09 +0900

    feat(sdk): change interface of environment build service

--- tt/sdk/environment_service/common/environment_build_service.py
+++ tt/sdk/environment_service/common/environment_build_service.py
@@ -1,10 +1,19 @@
 from __future__ import annotations
+
 from abc import abstractmethod
-from typing import TYPE_CHECKING, List, Literal, NewType, Union
+from typing import TYPE_CHECKING, List
 
 from tt.base.instantiate.instantiate import ServiceIdentifier, service_identifier
+from tt.sdk.environment_service.common.model.entity import (
+    EnvironmentCameraEntity,
+    EnvironmentLightEntity,
+    EnvironmentMachineEntity,
+)
 
 if TYPE_CHECKING:
+    import urllib.parse
+
+    from tt.sdk.environment_service.common.environment import IEnvironment
     from tt.sdk.environment_service.common.model.component import (
         MJCFPhysicsComponent,
         URDFPhysicsComponent,
@@ -13,22 +22,53 @@ if TYPE_CHECKING:
 
 
 @service_identifier("IEnvironmentBuildService")
-class IEnvironmentBuildService(ServiceIdentifier):
-
+class IEnvironmentBuildService(ServiceIdentifier["IEnvironmentBuildService"]):
     @abstractmethod
     def add_entity(
-        self, entity: MJCFPhysicsComponent | URDFPhysicsComponent | USDPhysicsComponent
+        self,
+        entity_or_uri: (
+            urllib.parse.SplitResult  # <- mostly for mjcf, urdf, or usc
+            | EnvironmentCameraEntity
+            | EnvironmentLightEntity
+            | EnvironmentMachineEntity
+        ),
     ): ...
 
     @abstractmethod
     def remove_entity(self, entity_id: str): ...
 
     @abstractmethod
-    def to_json(self) -> str: ...
+    def replace_entity(self, entity_id: str, render_uri: str, physics_uri: str): ...
+
+    @abstractmethod
+    def change_pos(self, entity_id: str): ...
+
+    @abstractmethod
+    def change_quat(self, entity_id: str): ...
+
+    @abstractmethod
+    def export_env_json(self) -> str: ...
+
+    @abstractmethod
+    def export_act_json(self) -> str: ...
+
+    @abstractmethod
+    def export_obs_json(self) -> str: ...
+
+    @abstractmethod
+    def load_mjcf(self, path: str) -> str: ...
+
+    @abstractmethod
+    def load_urdf(self, path: str) -> str: ...
+
+    @abstractmethod
+    def load_usd(self, path: str) -> str: ...
+
+    @abstractmethod
+    def build_env(self) -> IEnvironment: ...
 
 
 class EnvironmentBuildService(IEnvironmentBuildService):
-
     def __init__(self) -> None:
         self.entitys: (
             List[MJCFPhysicsComponent]
