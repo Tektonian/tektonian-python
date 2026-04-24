f981d590b474a6c943c78c6acd638fef1f0d66b2
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-04-24 20:51:25 +0900

    feat(sdk): add asset management service

---
+++ simulac/sdk/asset_service/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from .common.render_asset_resolver import RenderingAssetResolver, detect_asset_format
+
+__all__ = ["RenderingAssetResolver", "detect_asset_format"]
