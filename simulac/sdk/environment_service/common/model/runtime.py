e5bbdace0d38278cd27a67d075678811705df64d
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2026-04-28 21:46:32 +0900

    feat(sdk): add `Environment` runtime model

---
+++ simulac/sdk/environment_service/common/model/runtime.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+
+class State:
+    def __init__(self): ...
+
+
+class StuffRuntime:
+    def __init__(self): ...
+
+
+class MachineRuntime:
+    def __init__(self): ...
+
+
+class LightRuntime:
+    def __init__(self): ...
+
+
+class CameraRuntime:
+    def __init__(self): ...
